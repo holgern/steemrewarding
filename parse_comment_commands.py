fdc6921af2b01cdff92d3d88039f10f3bab21e64
Holger <holger@nahrstaedt.de>
2019-03-08 14:21:41 +0100

    add rewarding of paid-out post (older than 7days post) * when a already paid-out post should be upvoted, a comment  with beneficiaries is upvoted instead. * add short notice to dalayed vote tab

--- parse_comment_commands.py
+++ parse_comment_commands.py
@@ -1,10 +1,11 @@
-from beem.utils import formatTimeString, resolve_authorperm, construct_authorperm, addTzInfo
+from beem.utils import formatTimeString, resolve_authorperm, construct_authorperm, addTzInfo, derive_permlink
 from beem.nodelist import NodeList
 from beem.comment import Comment
 from beem import Steem
 from datetime import datetime, timedelta
 from beem.instance import set_shared_steem_instance
 from beem.blockchain import Blockchain
+from beem.account import Account
 import time 
 import json
 import os
@@ -13,14 +14,14 @@ import dataset
 import random
 from datetime import date, datetime, timedelta
 from dateutil.parser import parse
-from beem.constants import STEEM_100_PERCENT 
+from beem.constants import STEEM_100_PERCENT
 from steemrewarding.post_storage import PostsTrx
 from steemrewarding.command_storage import CommandsTrx
 from steemrewarding.vote_rule_storage import VoteRulesTrx
 from steemrewarding.pending_vote_storage import PendingVotesTrx
 from steemrewarding.config_storage import ConfigurationDB
 from steemrewarding.vote_storage import VotesTrx
-from steemrewarding.utils import isfloat, tags_included, tags_excluded
+from steemrewarding.utils import isfloat, tags_included, tags_excluded, valid_age
 from steemrewarding.version import version as rewardingversion
 from steemrewarding.command_parsing import parse_command
 
@@ -37,6 +38,7 @@ if __name__ == "__main__":
             config_data = json.load(json_data_file)
         # print(config_data)
         databaseConnector = config_data["databaseConnector"]
+        wallet_password = config_data["wallet_password"]
 
     start_prep_time = time.time()
     db = dataset.connect(databaseConnector)
@@ -52,6 +54,7 @@ if __name__ == "__main__":
     pendingVotesTrx = PendingVotesTrx(db)
     
     conf_setup = confStorage.get()
+    rewarding_account = "rewarding"
     last_command = conf_setup["last_command"]
     if last_command is None:
         last_command = datetime(1970,1,1,0,0,0)
@@ -88,7 +91,7 @@ if __name__ == "__main__":
     if "https://api.steemit.com" in node_list:
         node_list.remove("https://api.steemit.com")    
     stm = Steem(node=node_list, num_retries=5, call_num_retries=3, timeout=15, nobroadcast=nobroadcast) 
-    
+    stm.unlock(wallet_password)
     
     for command in commandsTrx.get_command_list(last_command):
         command_string = command["command"]
@@ -103,15 +106,32 @@ if __name__ == "__main__":
         c_comment = Comment(command["authorperm"], steem_instance=stm)
         voter = c_comment["author"]
         
+        voter_acc = Account(voter, steem_instance=stm)
+        posting_auth = False
+        for a in voter_acc["posting"]["account_auths"]:
+            if a[0] == "rewarding":
+                posting_auth = True        
+        
         if c_comment.is_main_post():
             c = c_comment
         else:
             c = Comment(construct_authorperm(c_comment["parent_author"], c_comment["parent_permlink"]), steem_instance=stm)
-        authorperm = c["authorperm"]
+        if not (c.is_pending() and valid_age(c)):
+            body = "The reward of this comment goes 100 %% to the author %s. This is done by setting the beneficiaries of this comment to 100 %%.\n\n" % (c["author"])
+            comment_beneficiaries = [{"account": c["author"], "weight": 10000}]
+
+            stm.post("rewarding %s" % c["author"], body, author=rewarding_account, reply_identifier=c_comment["authorperm"], beneficiaries=comment_beneficiaries)
+            time.sleep(3)
+            permlink = derive_permlink("rewarding %s" % c["author"], c_comment["parent_permlink"])
+            authorperm = construct_authorperm(rewarding_account, permlink)     
+            comment_timestamp = datetime.utcnow()
+        else:
+            authorperm = c["authorperm"]
+            comment_timestamp = c["created"].replace(tzinfo=None)
         
-        if command["valid"]:
+        if command["valid"] and posting_auth:
             
-            pending_vote = {"authorperm": authorperm, "voter": voter, "vote_weight": vote_weight, "vote_sbd": vote_sbd, "comment_timestamp": c["created"].replace(tzinfo=None),
+            pending_vote = {"authorperm": authorperm, "voter": voter, "vote_weight": vote_weight, "vote_sbd": vote_sbd, "comment_timestamp": comment_timestamp,
                             "vote_delay_min": vote_delay_min, "created": datetime.utcnow(), "min_vp": 0, "vote_when_vp_reached": True,
                             "vp_reached_order": 1, "max_net_votes": -1, "max_pending_payout": -1, "exclude_declined_payout": False,
                             "max_votes_per_day": -1, "max_votes_per_week": -1, "vp_scaler": 0, "leave_comment": False}
@@ -120,6 +140,8 @@ if __name__ == "__main__":
                                  "vote_delay_min": 0, "created": datetime.utcnow(), "min_vp": 0, "vote_when_vp_reached": False,
                                  "vp_reached_order": 1, "max_net_votes": -1, "max_pending_payout": -1, "exclude_declined_payout": False,
                                  "max_votes_per_day": -1, "max_votes_per_week": -1, "vp_scaler": 0, "leave_comment": False})
+        elif not posting_auth:
+            c_comment.reply("Please give rewarding the posting authory for letting it set beneficaries of your post/comment. https://app.steemconnect.com/authorize/@rewarding", author=account)         
 
     confStorage.update({"last_command": last_command})
     print("command parse script run %.2f s" % (time.time() - start_prep_time))
